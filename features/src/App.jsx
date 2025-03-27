80e79a03cbf55e2a00f445294ebe5c9408e8aea5
apasingh <104095503+apasingh@users.noreply.github.com>
2025-03-27 18:07:21 -0400

    added dependencies for google oauth

--- features/src/App.jsx
+++ features/src/App.jsx
@@ -8,30 +8,14 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+   
       <h1>Testing Calendar Integration</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-          {/* <iframe src="https://calendar.google.com/calendar/embed?src=c_1e92d15e451b9bb0032a9cc35a117f40844311302fab863f8f49f8f068a0ac7d%40group.calendar.google.com&ctz=America%2FNew_York" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe> */}
-        </p>
-      </div>
-      <p className="read-the-docs">
+      
+      {/* <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     <div>
       {/* <iframe src="https://calendar.google.com/calendar/embed?src=c_1e92d15e451b9bb0032a9cc35a117f40844311302fab863f8f49f8f068a0ac7d%40group.calendar.google.com&ctz=America%2FNew_York" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe> */}
-      test to see if this works
 
       {/* <!-- Elfsight Event Calendar | Untitled Event Calendar --> */}
       <script src="https://static.elfsight.com/platform/platform.js" async></script>
